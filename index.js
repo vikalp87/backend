c0a09f3099d65b0957acb20a9f35a96b5c41321a
vikalp87 <vikalp2949@gmail.com>
2023-12-21 21:00:56 +0530

    update in index.js

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ require('dotenv').config();
 const express=require('express');
 const app=express();
 const port=3000;
+
+const data={
+"name":"vikalp jain",
+"class":"9",
+"subject":"Maths"
+}
+
 app.get('/',(req,resp)=>{
     resp.send('hello backend')
 })
 app.get('/login',(req,resp)=>{
     resp.send('<h1>please login</h1>')
 })
-app.listen(process.env.PORT,()=>{
+app.get('/info',(req,resp)=>{
+    resp.json(data);
+})
+app.listen(port,()=>{
     console.log(`your server is running on ${port}`)
 })
